6b7132132be53687cf53baf25080e09c55651b22
Jesse Dyck <jessedyck@me.com>
2018-07-22 00:17:25 -0500

    Converted most functions to new API
    All except getAllDns;
    Upgrade lodash version

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ class hoverAPI {
 		
 		this._user = user;
 		this._pass = pass;
-		this._baseUrl = 'https://www.hover.com/api';
+		this._baseUrl = 'https://www.hover.com';
+		this._apiUrl = this._baseUrl + '/api/control_panel';
 		
 		// Captured from cookie in hover authentication request.
 		this._cookies = rp.jar();
@@ -50,7 +51,7 @@ class hoverAPI {
 	*/
 	_login () {
 		return this._rp({
-			uri: this._baseUrl + '/login',
+			uri: this._baseUrl + '/signin',
 			form: {username: this._user, password: this._pass},
 			method: 'POST'
 		})
@@ -84,7 +85,7 @@ class hoverAPI {
 	/**
 	* Retrieve individual domain in account
 	*
-	* @param {String} domain Domain identifier
+	* @param {String} domain Domain name
 	* @api public
 	*/
 	getDomain (domain) {
@@ -94,17 +95,17 @@ class hoverAPI {
 	/**
 	* Retrieve list of all dns records for particular domain
 	*
-	* @param {String} domain Domain identifier
+	* @param {String} domain Domain name
 	* @api public
 	*/
 	getDomainDns (domain) {
-		return this._hoverRequest({uri: '/domains/' + domain + '/dns'});
+		return this._hoverRequest({uri: '/dns/' + domain});
 	}
 	
 	/**
 	* Create a new A record under the specified domain
 	*
-	* @param {String} domain Domain identifier
+	* @param {String} domain Domain name
 	* @param {String} subdomain Subdomain of record
 	* @param {String} ip IP Address of record
 	* @api public
@@ -112,11 +113,15 @@ class hoverAPI {
 	createARecord (domain, subdomain, ip) {
 		var req = {
 			method: 'POST',
-			uri: '/domains/' + domain + '/dns',
+			uri: '/dns',
 			body: {
-				name: subdomain,
-				type: 'A',
-				content: ip
+				dns_record: {
+					name: subdomain,
+					content: ip,
+					type: 'A',
+					ttl: '900'
+				},
+				'id': 'domain-' + domain
 			}
 		};
 		
@@ -126,7 +131,7 @@ class hoverAPI {
 	/**
 	* Create a new MX record under the specified domain
 	*
-	* @param {String} domain Domain identifier
+	* @param {String} domain Domain name
 	* @param {String} subdomain Subdomain of record
 	* @param {String} priority Priority of record
 	* @param {String} ip IP Address of record
@@ -135,11 +140,15 @@ class hoverAPI {
 	createMXRecord (domain, subdomain, priority, ip) {
 		var req = {
 			method: 'POST',
-			uri: '/domains/' + domain + '/dns',
+			uri: '/dns',
 			body: {
-				name: subdomain,
-				type: 'MX',
-				content: [priority, ip].join(' ')
+				dns_record: {
+					name: subdomain,
+					content: [priority, ip].join(' '),
+					type: 'MX',
+					ttl: '900'
+				},
+				id: 'domain-' + domain
 			}
 		};
 		
@@ -148,17 +157,27 @@ class hoverAPI {
 	
 	/**
 	* Update an existing domain record
+	* TODO
 	*
-	* @param {String} dns DNS identifier
+	* @param {String} domain Domain name
+	* @param {String} dns DNS identifier (dns########)
 	* @param {String} ip New IP Address of record
 	* @api public
 	*/
-	updateDomainDns (dns, ip) {
+	updateDomainDns (domain, dns, ip) {
 		var req = {
 			method: 'PUT',
-			uri: '/dns/' + dns,
+			uri: '/dns',
 			body: {
-				content: ip
+				domain: {
+					id: 'domain-' + domain,
+					dns_records: [{
+						id: dns
+					}]
+				},
+				fields: {
+					content: ip
+				}
 			}
 		};
 		
@@ -167,12 +186,58 @@ class hoverAPI {
 	
 	/**
 	* Remove an existing dns record
+	* TODO
 	*
-	* @param {String} dns DNS identifier
+	* @param {String} domain Domain name
+	* @param {String} dns DNS identifier (dns########)
 	* @api public
 	*/
-	removeDns (dns) {
-		return this._hoverRequest({method: 'DELETE', uri: '/dns/' + dns});
+	removeDns (domain, dns) {
+		var req = {
+			method: 'DELETE',
+			uri: '/dns',
+			body: {
+				domains: [{
+					id: 'domain-' + domain,
+					name: domain,
+					dns_records: [dns]
+				}]
+			}
+		};
+
+		return this._hoverRequest(req);
+	}
+
+	getSubdomainIdentifiers (domain, subdomain, recordtype) {
+		if (!_.isString(domain) || domain == '') 
+			return Promise.reject('Invalid domain supplied.');
+
+		if (!_.isString(subdomain) || subdomain == '')
+			return Promise.reject('Invalid subdomain supplied.');
+
+		if (!_.isString(recordtype) || recordtype == '')
+			return Promise.reject('Invalid recordtype supplied.');
+
+		return this.getDomainDns(domain)
+			.catch( () => Promise.reject('Could not find domain ' + domain + ' or connection error.'))
+			.then( domainDNS => {
+				this._log('Got DNS Entries:');
+				this._log(domainDNS[0].dns);
+
+				let matchingEntries = [];
+
+				// Find entries that match the parameters specified
+				matchingEntries = domainDNS[0].dns.filter( entry => entry.name.toLowerCase() == subdomain.toLowerCase() && entry.type.toLowerCase() == recordtype.toLowerCase() );
+
+				if (matchingEntries.length == 0)
+					return Promise.reject('No matching subdomain and record type found.');
+
+				// Only need the ID to issue the API request
+				matchingEntries = matchingEntries.map(entry => entry.id);
+				this._log('Matching IDs:' + matchingEntries.join(', '));
+
+				return Promise.resolve(matchingEntries);
+			});
 	}
 	
 	/**
@@ -184,7 +249,7 @@ class hoverAPI {
 	* @api private
 	*/
 	_hoverRequest (req) {
-		req.uri = this._baseUrl + req.uri;
+		req.uri = this._apiUrl + req.uri;
 		
 		this._log('Compiled request:');
 		this._log(req);
@@ -198,10 +263,10 @@ class hoverAPI {
 			.then (data => {
 				this._log('Got response:');
 				this._log(data);
-				const key = _.without(_.keys(data), 'succeeded');
-				const value = data[key] ? data[key] : data.succeeded;
 				
-				return Promise.resolve(value);
+				_.unset(data, 'succeeded');
+				
+				return Promise.resolve(_.toArray(data));
 			})
 			.catch ( err => {
 				return Promise.reject(err.message);
